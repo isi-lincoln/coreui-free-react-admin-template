4d6bad85b36ead96d31f00bb8879d00f085d99db
Lincoln Thurlow <lincoln@isi.edu>
2024-05-09 10:42:27 -0700

    a rough statistics page
    Signed-off-by: Lincoln Thurlow <lincoln@isi.edu>

--- src/views/pages/misc/Stats.js
+++ src/views/pages/misc/Stats.js
@@ -21,6 +21,36 @@ import {
 import { AppSidebar, AppFooter, AppHeader } from '../../../components/index'
 
 
+// TODO: percent pull from top using REST/GET to api server
+const tempArr = Data.map((data) => {
+  if(data.data.id === "orchestrator"){
+    return {
+      ...data,
+      title: data.data.id,
+      value: "Utilization",
+      percent: Math.floor(Math.random()* (100 - 90 + 1)) + 90,
+      color: "danger",
+    }
+  } else if(data.data.id === "sdcore1" || data.data.id == "sdcore2"){
+    return {
+      ...data,
+      title: data.data.id,
+      value: "Utilization",
+      percent: Math.floor(Math.random()* (60 - 40 + 1)) + 40,
+      color: "warning",
+    }
+  } else {
+    return {
+      ...data,
+      title: data.data.id,
+      value: "Utilization",
+      percent: Math.floor(Math.random()* (12 - 1 + 1)) + 1,
+      color: "success",
+    }
+  }
+});
+
+
 const Stats = () => {
 
   // TODO: Need to change the progress to be another type so that it displays correctly
@@ -39,16 +69,16 @@ const Stats = () => {
             xl={{ cols: 5 }}
             className="mb-2 text-center"
           >
-            {Data.map((item, index, items) => (
+            {tempArr.map((item, index, items) => (
               <CCol
                 className={classNames({
                   'd-none d-xl-block': index + 1 === items.length,
                 })}
                 key={index}
               >
-                <div className="text-body-secondary">{item.id}</div>
+                <div className="text-body-secondary">{item.title}</div>
                 <div className="fw-semibold text-truncate">
-                  {item.cores} ({item.memory}%)
+                  {item.value} ({item.percent}%)
                 </div>
                 <CProgress thin className="mt-2" color={item.color} value={item.percent} />
               </CCol>
@@ -56,6 +86,23 @@ const Stats = () => {
           </CRow>
         </CCardBody>
       </CCard>
+
+      <br />
+
+      <CRow>
+        <CCol xs>
+          <CCard className="mb-4">
+            <CCardHeader>Node Details</CCardHeader>
+            <CCardBody>
+              <CRow>
+                <CCol xs={12} md={6} xl={6}>
+                  <pre>{JSON.stringify(Data, null, 2)}</pre>
+                </CCol>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
 
         </div>
         <AppFooter />
